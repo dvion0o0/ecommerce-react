1b4d0bc35e094c272efe01dc9a10970408e59221
Dv <dv@Dvs-MacBook-Pro.local>
2021-03-08 21:19:06 +0530

    router added

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export { default as Home } from "./HomePage";
+export { default as Products } from "./ProductsPage";
+export { default as About } from "./AboutPage";
+export { default as Cart } from "./CartPage";
+export { default as Checkout } from "./CheckoutPage";
+export { default as PrivateRoute } from "./PrivateRoute";
+export { default as Error } from "./ErrorPage";
+export { default as AuthWrapper } from "./AuthWrapper";
+export { default as SingleProduct } from "./SingleProductPage";
