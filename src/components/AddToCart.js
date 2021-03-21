50f370a9a39ff2dba2987da5f41fa65a01faaa67
Dv <dv@Dvs-MacBook-Pro.local>
2021-03-22 03:40:04 +0530

    authentication added

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -9,6 +9,7 @@ const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
+  const { addToCart } = useCartContext();
   const increase = () => {
     setAmount((oldAmount) => {
       let tempAmount = oldAmount + 1;
@@ -56,7 +57,11 @@ const AddToCart = ({ product }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to="/cart" className="btn">
+        <Link
+          to="/cart"
+          className="btn"
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
           add to cart
         </Link>
       </div>
--- src/index.js
+++ src/index.js
@@ -8,11 +8,25 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// dvion0o0.us.auth0.com
+// UeSzKaqwKJkw3y0yE9VWDCTBEx7RuqpK
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dvion0o0.us.auth0.com"
+    clientId="UeSzKaqwKJkw3y0yE9VWDCTBEx7RuqpK"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
